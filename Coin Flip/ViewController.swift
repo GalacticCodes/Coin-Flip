fa35bc6a0faa65ef66b624bc20f63ac620aba93c
Matthew Morgan <mmorganc@live.com>
2021-10-21 13:27:42 -0400

    coin flip app complete

--- Coin Flip/ViewController.swift
+++ Coin Flip/ViewController.swift
@@ -11,12 +11,24 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var image: UIImageView!
     
+     
+    //create an array/list to store two images
+    
+    var imageList = [UIImage(named:"heads"), UIImage(named:"tails")]
+    
+    
     
     @IBAction func click(_ sender: UIButton) {
         
         //Who.What = value
+            //access value from the array
         
-        image.image = UIImage(named:"heads")
+        //function -> Random int.random
+        // generate random position when you click on the button
+        
+        let randomPosition = Int.random(in: 0...1)
+        
+        image.image = imageList[randomPosition]
 
     }
     
